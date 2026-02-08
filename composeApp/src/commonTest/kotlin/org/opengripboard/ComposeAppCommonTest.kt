2c50a92a17e213ba4683782fafd6836e30362f12
Jonas Burkhard <jonas@burkhard.one>
2026-02-08 19:04:34 +0100

    add kmp template

---
+++ composeApp/src/commonTest/kotlin/org/opengripboard/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.opengripboard
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
