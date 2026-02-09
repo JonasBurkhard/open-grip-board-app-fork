81ac810609d9213314cef98b0f6438f8c0cf4772
Jonas Burkhard <jonas@burkhard.one>
2026-02-09 18:41:56 +0100

    refactor: change clock provider

--- composeApp/src/commonMain/kotlin/org/opengripboard/model/StatisticsManager.kt
+++ composeApp/src/commonMain/kotlin/org/opengripboard/model/StatisticsManager.kt
@@ -1,23 +1,18 @@
 package org.opengripboard.model
 
+import org.opengripboard.data.objects.Training
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import kotlinx.datetime.DatePeriod
-import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
-import org.opengripboard.data.objects.Training
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.minus
 import kotlinx.datetime.plus
-import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
-import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration
 
 class StatisticsManager {
     var trainingDurationLastTwoWeeks = mutableStateListOf<Pair<LocalDate, Duration>>()
@@ -31,8 +26,8 @@ class StatisticsManager {
 
     fun recalculateFor(page: PageId, pastTrainings: List<Training>) {
         if (page != PageId.Dashboard) return
-        val timeZone = TimeZone.currentSystemDefault()
-        val today: LocalDate = Clock.System.now().toLocalDateTime(timeZone).date
+        val today: LocalDate =
+            Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
         val startDate: LocalDate = today.minus(DatePeriod(days = 14))
         val recentTrainings = pastTrainings.filter { it.date.date in startDate..today }
 
