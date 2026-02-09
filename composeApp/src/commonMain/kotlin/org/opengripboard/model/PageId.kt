69925c9365c9bd28c7c16758e382767a81d64882
Jonas Burkhard <jonas@burkhard.one>
2026-02-09 18:23:58 +0100

    feat: add page id enum

---
+++ composeApp/src/commonMain/kotlin/org/opengripboard/model/PageId.kt
@@ -0,0 +1,8 @@
+package org.opengripboard.model
+
+enum class PageId(val display: String) {
+    Dashboard("Dashboard"),
+    ConnectBoard("Connect a new Hangboard"),
+    PastTrainings("Your past trainings"),
+    RecordingData("Recording data"),
+}
