fcb87f36af1b51932f82cdabf93fccaca90c68df
Jonas Burkhard <jonas@burkhard.one>
2026-02-09 18:42:57 +0100

    feat: add icons dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -52,6 +52,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
+            implementation(libs.material.icons.extended)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
