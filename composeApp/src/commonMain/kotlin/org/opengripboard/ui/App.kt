642829ae6206f6086a1555ce00402206955896f8
Jonas Burkhard <jonas@burkhard.one>
2026-02-09 14:58:19 +0100

    feat: add viewContent to app

--- composeApp/src/commonMain/kotlin/org/opengripboard/ui/App.kt
+++ composeApp/src/commonMain/kotlin/org/opengripboard/ui/App.kt
@@ -1,50 +1,68 @@
 package org.opengripboard.ui
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 
-import opengripboard.composeapp.generated.resources.Res
-import opengripboard.composeapp.generated.resources.compose_multiplatform
-import org.opengripboard.Greeting
+import org.opengripboard.model.OgbViewModel
+import org.opengripboard.model.PageId
+import org.opengripboard.ui.preview.ModelProvider
+import org.opengripboard.ui.views.ConnectBoard
+import org.opengripboard.ui.views.Dashboard
+import org.opengripboard.ui.views.PastTrainings
+import org.opengripboard.ui.views.RecordingData
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 @Preview
-fun App() {
+fun App(@PreviewParameter(ModelProvider::class) model: OgbViewModel) {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Scaffold { padding ->
+            Column(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .safeContentPadding()
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(modifier = Modifier.height(50.dp).padding(padding))
+                ViewContent(model)
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        }
+    }
+}
+
+@Composable
+fun ViewContent(model: OgbViewModel) {
+    when (model.navigation.currentPage) {
+        PageId.Dashboard -> {
+            Dashboard()
+        }
+
+        PageId.ConnectBoard -> {
+            ConnectBoard()
+        }
+
+        PageId.PastTrainings -> {
+            PastTrainings()
+        }
+
+        PageId.RecordingData -> {
+            RecordingData()
         }
     }
 }
