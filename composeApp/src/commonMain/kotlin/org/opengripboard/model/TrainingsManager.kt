5fc12cb3350cfd2ee0c225ef46c9112d29efb385
Jonas Burkhard <jonas@burkhard.one>
2026-02-11 23:06:06 +0100

    feat: add training from readings

--- composeApp/src/commonMain/kotlin/org/opengripboard/model/TrainingsManager.kt
+++ composeApp/src/commonMain/kotlin/org/opengripboard/model/TrainingsManager.kt
@@ -1,13 +1,19 @@
 package org.opengripboard.model
 
 import androidx.compose.runtime.mutableStateListOf
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.opengripboard.data.objects.Training
+import kotlin.time.Clock
+import kotlin.time.Duration
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 class TrainingsManager {
     var pastTrainings = mutableStateListOf<Training>()
         private set
 
-    fun onDeletePressed(id: Int) {
+    fun onDeletePressed(id: String) {
         pastTrainings.removeAll { training -> training.id == id }
     }
 
@@ -17,5 +23,20 @@ class TrainingsManager {
 
     fun addTrainings(newTrainings: List<Training>) {
         newTrainings.forEach { training -> pastTrainings.add(training) }
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    fun addTrainingFromReadings(readings: List<Int>, duration: Duration) {
+        if (readings.isEmpty()) {
+            return
+        }
+        addTraining(
+            Training(
+                id = Uuid.random().toString(),
+                date = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()),
+                dataPoints = readings,
+                duration = duration,
+            )
+        )
     }
 }
