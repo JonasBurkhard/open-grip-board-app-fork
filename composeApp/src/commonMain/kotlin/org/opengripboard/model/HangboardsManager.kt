e7669e1f66e571d6c4f3661983e6cfbae7131e3b
Jonas Burkhard <jonas@burkhard.one>
2026-02-11 23:06:30 +0100

    feat: add start and stop recording logic

--- composeApp/src/commonMain/kotlin/org/opengripboard/model/HangboardsManager.kt
+++ composeApp/src/commonMain/kotlin/org/opengripboard/model/HangboardsManager.kt
@@ -4,11 +4,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import org.opengripboard.data.objects.Hangboard
+import kotlin.time.Clock
+import kotlin.time.Duration
+import kotlin.time.Instant
 
 class HangboardsManager {
     var isRecording by mutableStateOf(false)
         private set
+    private var recordingStartTime: LocalDateTime? = null
     var currentReadings = mutableStateListOf<Int>()
         private set
     var availableHangboards = mutableStateListOf<Hangboard>()
@@ -22,11 +30,19 @@ class HangboardsManager {
     }
 
     fun onStartRecording() {
+        currentReadings.clear()
+        recordingStartTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
         isRecording = true
     }
 
-    fun onStopRecording() {
+    fun onStopRecording(): Duration {
         isRecording = false
+        recordingStartTime?.let { startTime ->
+            return Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+                .toInstant(TimeZone.currentSystemDefault()) -
+                    startTime.toInstant(TimeZone.currentSystemDefault())
+        }
+        return Duration.ZERO
     }
 
     fun addHangboard(newHangboard: Hangboard) {
@@ -36,4 +52,14 @@ class HangboardsManager {
     fun addHangboards(newHangboards: List<Hangboard>) {
         newHangboards.forEach { hangboard -> availableHangboards.add(hangboard) }
     }
+
+    fun onNewMqttMessage(message: String) {
+        if (isRecording) {
+            val reading = message.toIntOrNull()
+            if (reading != null) {
+                currentReadings.add(reading)
+            }
+        }
+    }
+
 }
