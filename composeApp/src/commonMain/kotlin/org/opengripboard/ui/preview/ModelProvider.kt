e00956ab3ba79f38954d507294ffc43d94b81eb4
Jonas Burkhard <jonas@burkhard.one>
2026-02-11 18:15:46 +0100

    feat: update preview

--- composeApp/src/commonMain/kotlin/org/opengripboard/ui/preview/ModelProvider.kt
+++ composeApp/src/commonMain/kotlin/org/opengripboard/ui/preview/ModelProvider.kt
@@ -1,17 +1,50 @@
 package org.opengripboard.ui.preview
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.opengripboard.data.objects.Training
 import org.opengripboard.model.OgbViewModel
 import org.opengripboard.model.PageId
 import kotlin.apply
+import kotlin.random.Random
+import kotlin.time.Clock
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.minutes
 
 class ModelProvider : PreviewParameterProvider<OgbViewModel> {
     override val values: Sequence<OgbViewModel> = sequenceOf(
-        OgbViewModel().apply{
-            
+        OgbViewModel().apply {
+
         },
-        OgbViewModel().apply{
+        OgbViewModel().apply {
+            trainings.addTrainings(
+                List(28) { id ->
+                    Training(
+                        id = id,
+                        date = Clock.System.now().minus(Random.nextInt(0, 15).days)
+                            .toLocalDateTime(TimeZone.currentSystemDefault()),
+                        dataPoints = List(20) { Random.nextInt(5000, 60000) },
+                        duration = Random.nextInt(1, 120).minutes
+                    )
+                }
+            )
             navigation.navigate(PageId.PastTrainings)
+        },
+        OgbViewModel().apply {
+            trainings.addTrainings(
+                List(28) { id ->
+                    Training(
+                        id = id,
+                        date = Clock.System.now().minus(Random.nextInt(0, 15).days)
+                            .toLocalDateTime(TimeZone.currentSystemDefault()),
+                        dataPoints = List(20) { Random.nextInt(5000, 60000) },
+                        duration = Random.nextInt(1, 120).minutes
+                    )
+                }
+            )
+            navigation.navigate(PageId.Dashboard)
         },
     )
 }
