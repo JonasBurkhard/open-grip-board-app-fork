50ade71c6d6bc1992f1b35a05b917e42fe7e6982
Jonas Burkhard <jonas@burkhard.one>
2026-06-20 19:27:23 +0200

    feat: add flashlight to qr scanner

--- composeApp/src/androidMain/kotlin/org/opengripboard/ui/components/QrScannerPlatform.android.kt
+++ composeApp/src/androidMain/kotlin/org/opengripboard/ui/components/QrScannerPlatform.android.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -55,7 +56,7 @@ actual fun QrScannerComponent(
                 },
                 modifier = Modifier.fillMaxSize(),
                 scanningMode = ScanningMode.PARALLEL,
-                isFlashEnabled = remember { mutableStateOf(flashIsEnabled) },
+                isFlashEnabled = rememberUpdatedState(flashIsEnabled),
                 linearZoom = remember { mutableStateOf(0f) },
             )
         }
